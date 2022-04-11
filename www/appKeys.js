4bfce0def4e4c27255280ee377d3946a0033cf11
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-04-11 06:25:24 +0100

    AppKeys Path Fix

--- www/appKeys.js
+++ www/appKeys.js
@@ -1,3 +1,3 @@
 export const APIKey = 'f7857fa32f5f4675bd49d3efb9ab3491';
-export const AuthHeader = 'NDAwMjI6WEEwYXA0MnlTR29tM1kwLVREMFRuWDFCQjNRU1lmTXdGdjRiTTcwUGd5OA==';
+export const AuthHeader = 'NDAwMjQ6SnNnQms0UU5JUGtWMXlXY0MzZUpPZGxCRmsyVzhnU0J0UFZITVgwcTFvWQ==';
 export const HomeURL = 'http://86.2.10.33:4645/D2-Synergy/www/';
